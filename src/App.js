d3263c1f11f5d3343388fc6fa3800793f51149da
knitterJ <knitter.m.jacob@gmail.com>
2020-07-09 01:14:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+export default App;
+
