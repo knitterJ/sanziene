05e48ff015ad3cde9b1eae9c3411cbeb2c307d84
knitterJ <knitter.m.jacob@gmail.com>
2020-07-09 03:24:53 +0200

    preliminary state of getting to the json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
+import React, {  } from 'react';
+
+
 
 class App extends React.Component {
+
+  state = 
+  {
+   
+    name: "Jacob", 
+    languages: ["PHP", "C^"],
+    data: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/api/cassetts/all')
+      .then(response => response.json())
+      .then(data => {
+        console.log({data})
+        this.setState({ data })
+      }
+      );
+  }
+
   render() {
     return (
       <div>
-        Hello {this.props.name}
+        Hello {this.state.name}
+        <p>languages: {this.state.languages[0]}</p>
+        <p>And finally: {this.state.data.info}</p>
       </div>
     );
   }
